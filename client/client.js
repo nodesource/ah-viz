df5a8ee4fd48bdfac87bd59dcc30945282f0675d
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-06 15:04:29 -0600

    client: datamunger working and visualizer prepared

--- client/client.js
+++ client/client.js
@@ -1,2 +1,24 @@
-const raphael = require('raphael')
-console.log({ raphael })
+const data = require('../samples/one-file.read-file.processed.json')
+const mungeData = require('../lib/munge-data')
+
+// eslint-disable-next-line no-unused-vars
+function inspect(obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true))
+}
+
+const { lifetime, operations } = mungeData(data)
+
+inspect({ lifetime, operations })
+
+//
+// Visualization
+//
+const Visualizer = require('../lib/visualizer')
+const width = 650
+const height = 340
+const ratioX = width / lifetime
+const ratioY = 1
+const svg = document.createElement('svg')
+document.body.appendChild(svg)
+
+const visualizer = new Visualizer({ svg, width, height, ratioX, ratioY })
