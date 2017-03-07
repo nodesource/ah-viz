10c5e5afcc0d1a4faec0ee0b129ad32f8cb3cdd3
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-07 16:58:42 +0100

    viz: drawing operation rectangle

--- client/client.js
+++ client/client.js
@@ -1,14 +1,10 @@
 const data = require('../samples/one-file.read-file.processed.json')
 const mungeData = require('../lib/munge-data')
-
-// eslint-disable-next-line no-unused-vars
-function inspect(obj, depth) {
-  console.error(require('util').inspect(obj, false, depth || 5, true))
-}
+const debugData = require('debug')('viz:data')
 
 const { lifetime, operations } = mungeData(data)
 
-inspect({ lifetime, operations })
+debugData({ lifetime, operations })
 
 //
 // Visualization
@@ -22,3 +18,4 @@ const svg = document.createElement('svg')
 document.body.appendChild(svg)
 
 const visualizer = new Visualizer({ svg, width, height, ratioX, ratioY })
+visualizer.render(operations)
