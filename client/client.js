163b3af9f76d793769d7419645824e6c719b80cf
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-07 17:33:03 +0100

    operation: displaying operation details on click

--- client/client.js
+++ client/client.js
@@ -1,5 +1,7 @@
 const data = require('../samples/one-file.read-file.processed.json')
+
 const mungeData = require('../lib/munge-data')
+const Operation = require('../lib/operation')
 const debugData = require('debug')('viz:data')
 
 const { lifetime, operations } = mungeData(data)
@@ -16,6 +18,19 @@ const ratioX = width / lifetime
 const ratioY = 1
 const svg = document.createElement('svg')
 document.body.appendChild(svg)
+const details = document.createElement('div')
+document.body.appendChild(details)
 
-const visualizer = new Visualizer({ svg, width, height, ratioX, ratioY })
+const visualizer = new Visualizer({
+    svg
+  , width
+  , height
+  , ratioX
+  , ratioY
+  , onoperationClicked
+})
 visualizer.render(operations)
+
+function onoperationClicked({ operation, name }) {
+  details.innerHTML = new Operation({ operation, name }).render()
+}
